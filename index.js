a02dcf5d52c82dc24d9a60ee30e3a01d840ac863
Lsong <song940@gmail.com>
2026-04-28 12:22:28 +0800

    update

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ const App = ({ questions, typeDescriptions, dimensionLabels, dimensionLetters })
 }
 
 ready(async () => {
-  const app = document.getElementById('mbti-app');
+  const app = document.getElementById('app');
   const data = await loadJSON('data.json');
   const { questions, typeDescriptions, dimensionLabels, dimensionLetters } = data;
   render(h(App, { questions, typeDescriptions, dimensionLabels, dimensionLetters }), app);
